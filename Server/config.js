f810310f54aaf0e0efdf317df9e24ae3bbe2e131
edmundchan70 <edmundchan70@gmail.com>
2022-12-20 13:44:08 -0500

    first commit

---
+++ Server/config.js
@@ -0,0 +1,23 @@
+const loginData= {
+    client: 'pg',
+    port: 5431,
+    username: "edmundchan"
+    
+}
+module.exports = {
+    ps5_pic :"https://twinfinite.net/2020/06/ps5-official-pictures/ps5-4-2/",
+     title : "PS5",
+     price :150 ,
+     rating:5,
+     Server:"http://localhost:3002"
+}
+
+/*
+ Create table product( 
+     id SERIAL PRIMARY KEY ,
+        image varchar ,      
+        title varchar  ,
+        price int ,   
+       rating int );
+       
+       */
