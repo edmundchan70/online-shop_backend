a2e43cd8f64a7bbc513777366ca5f1580d8c4f04
edmundchan70 <edmundchan70@gmail.com>
2022-12-20 14:25:20 -0500

    updaed

---
+++ Server/index.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const express = require('express')
+const app = express();
+const cors = require('cors');
+ 
+const ports = 8080;
+
+const data = require('./config');
+const { allowedUrl } = require('./config');
+const api = require("./api/api");
+app.use(cors({origin:allowedUrl}));
+app.use(express.json());
+const server = data.Server;
+app.use("/api" , api);
+ 
+app.listen(ports , ()=>{
+  console.log('listening on port ' + ports);
+})
+ 
+module.exports = app;
+
+
+ 
+
+/*
+CREATE TABLE CUSTOMER (
+  userId SERIAL PRIMARY KEY ,
+  email varchar ,
+  password varchar
+)
+
+*/
